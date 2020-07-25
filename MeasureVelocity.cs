7abc5d07f6d6443d98846c78ed6e574697dcc48d
Anshul692 <44148237+Anshul692@users.noreply.github.com>
2020-07-25 06:07:18 +0530

    Create MeasureVelocity.cs

---
+++ MeasureVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureVelocity : MonoBehaviour
+{
+    [SerializeField] Vector3 coords;
+    [SerializeField] float speed;
+    Vector3 oldCoords;
+
+    void FixedUpdate()
+    {
+        coords = transform.position;
+        speed = Vector3.Distance(coords, oldCoords) / 50f;
+        print(speed + " m/s");
+        oldCoords = coords;
+    }
+}
